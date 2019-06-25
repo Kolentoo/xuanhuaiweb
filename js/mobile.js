a41d0954a90c158a4cebfddae05bf9231185b01b
kolento <515835543@qq.com>
2019-06-25 10:27:58 +0800

    fixed
    Signed-off-by: kolento <515835543@qq.com>

--- js/mobile.js
+++ js/mobile.js
@@ -2,6 +2,21 @@ $(function(){
 
     downloadBtn();
 
+
+    var u = navigator.userAgent, app = navigator.appVersion;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
+    var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    if (isAndroid) {
+       //这个是安卓操作系统
+       window.location.href='http://elite-league.oss-cn-shanghai.aliyuncs.com/apk/hsuanhuai_service.apk'
+       return 'az'
+    }
+    if (isIOS) {
+　　　　//这个是ios操作系统
+        window.location.href='https://apps.apple.com/cn/app/id1459607274'
+        return 'ios'
+    }
+
 })
 
 // 点击下载提示
@@ -9,28 +24,44 @@ function downloadBtn(){
     $('.btn').on('click',function(){
         if(phoneSystem()==='az'){
             $('.ios-msg').addClass('hide')
+            
             $('.anzhuo-msg').removeClass('hide')
         }else{
             $('.ios-msg').removeClass('hide')
             $('.anzhuo-msg').addClass('hide')
         }
-        $('.msg').addClass('msgon')
+        if(isWeiXin()){
+            $('.msg').addClass('msgon')
+        }else{
+            $('.msg').removeClass('msgon')
+        }
+        
     })
     
 }
 
+// 系统判断
 function phoneSystem(){
     var u = navigator.userAgent, app = navigator.appVersion;
     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
     var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
     if (isAndroid) {
        //这个是安卓操作系统
-       alert('安卓')
+       window.location.href='http://elite-league.oss-cn-shanghai.aliyuncs.com/apk/hsuanhuai_service.apk'
        return 'az'
     }
     if (isIOS) {
 　　　　//这个是ios操作系统
-        alert('ios')
         return 'ios'
+    }
+}
+
+// 微信判断
+function isWeiXin(){
+    var ua = window.navigator.userAgent.toLowerCase();
+    if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+        return true;
+    }else{
+        return false;
     }
 }
